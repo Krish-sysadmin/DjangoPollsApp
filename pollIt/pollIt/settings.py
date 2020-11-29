33042480fe91aee17453472e890efaa8dc808c70
Krish-sysadmin <krishjain02939@gmail.com>
2020-11-29 15:20:03 +0100

    Remove junk files

--- pollIt/pollIt/settings.py
+++ pollIt/pollIt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xv=v0-@##)opn%d9!rn=pwyx9d@=aq^5_1nd)+$@nodz6)mp)h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://pollingkrish.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,4 +119,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticsfiles')
 STATIC_URL = '/static/'
